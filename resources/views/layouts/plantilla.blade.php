93d16452a0cc592aaac7b05d1880435da39c298a
JohnMRivera <johnrivera310802@gmail.com>
2022-10-28 16:45:56 -0500

    footer

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -33,6 +33,12 @@
 
     @yield('contenido')
 
+    <footer>
+      <div class="alert bg-danger text-light text-center mt-5">
+        Juan Manuel Rivera Olvera 2022 CopyRight ©
+      </div>
+    </footer>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
