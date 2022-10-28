db2a8e091f42b26820e9441c4dd60f11cbee9322
JohnMRivera <johnrivera310802@gmail.com>
2022-10-28 16:39:17 -0500

    Añadir tablas

--- resources/views/tabla.blade.php
+++ resources/views/tabla.blade.php
@@ -8,6 +8,36 @@
         <header>
             <h1 class="text-center mt-5">Tabla</h1>
         </header>
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col"></th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Apellido</th>
+                <th scope="col">Grupo</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Juan</td>
+                <td>Rivera</td>
+                <td>S171</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Brian</td>
+                <td>Soto</td>
+                <td>S171</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Brayan</td>
+                <td>Carranza</td>
+                <td>S171</td>
+              </tr>
+            </tbody>
+          </table>
     </main>
 
 @endsection
